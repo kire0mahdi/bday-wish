35b20cc3b7d33ccde6807c641c81aa687373f7a7
Mahdi Hasan <106861619+kire0mahdi@users.noreply.github.com>
2025-01-17 23:05:49 +0600

    fixed NaN Error & Updated UI

--- scripts.js
+++ scripts.js
@@ -8,3 +8,19 @@ window.onload = function() {
     // Set the current year in the footer
     document.getElementById('year').textContent = new Date().getFullYear();
 };
+
+// Music Event
+const backgroundMusic = document.getElementById("backgroundMusic");
+const musicButton = document.getElementById("music");
+let isPlaying = false;
+
+function toggleMusic() {
+    if (isPlaying) {
+        backgroundMusic.pause();
+        musicButton.textContent = "Play Music";
+    } else {
+        backgroundMusic.play();
+        musicButton.textContent = "Pause Music";
+    }
+    isPlaying = !isPlaying;
+}
